9de5fc32cc16810ec1ccdd7bc98bcc4bdd1393d5
wassim-saadallah <wassim.saadalla@gmail.com>
2020-06-24 15:34:13 +0100

    trying overlays (not succeeding)

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,27 +1,29 @@
 <script>
-  import * as L from "leaflet";
-  import { onMount } from "svelte";
-  onMount(async () => {
-    const map = L.map("map", {
-      center: [43.00, -79.00],
-      zoom: 9,
-    })
-    
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map)
-    
-    map.invalidateSize()
-  })
+	import * as L from 'leaflet';
+	import { onMount } from 'svelte';
+	import Login from './Login.svelte';
+	onMount(async () => {
+		const map = L.map('map', {
+			center: [43.0, -79.0],
+			zoom: 9,
+		});
 
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(map);
+
+		map.invalidateSize();
+	});
 </script>
 
 <style>
-  main {
-    width: 100%;
-    height: 100%;
-  }
+	main {
+		position: relative;
+	}
 </style>
 
-<main id="map"></main>
+<main>
+	<div id="map" />
+	<Login />
+</main>
---
+++ client/src/Login.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<style>
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: 314159;
+		pointer-events: none;
+	}
+	.overlay .btn {
+		pointer-events: initial;
+	}
+</style>
+
+<div class="row-fluid overlay asd">
+	<div class="span1">
+		<button class="btn btn-primary">Button</button>
+	</div>
+	<div class="span2">
+		<button class="btn">Button</button>
+	</div>
+</div>
